fd4bce48be6e324c806e4fb8e551e4b18d6d8930
Philip Claren <kobe@mixxt.net>
2013-12-02 15:05:39 +0100

    player details: bar chart now with textuell percentage | ranking: only players who have played at least one game are listed

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,10 +1,11 @@
-if(!Players.findOne() && !Games.findOne()) {
-  Players.insert({name: 'Philip',    gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Christoph', gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Axel',      gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Ben',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Oli',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Pan',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Daniel',    gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
-  Players.insert({name: 'Roman',     gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+if(!Players.findOne()) {
+  Players.insert({name: 'Kobe',        gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Christoph',   gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Aggel',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Benni',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Hundefrisör', gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Oli',         gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Pan',         gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Daniel',      gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
+  Players.insert({name: 'Roman',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: []});
 }
