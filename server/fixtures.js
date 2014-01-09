361f01b7af190f2e6a36d09eb3f528edacedbe26
Philip Claren <kobe@mixxt.net>
2014-01-09 18:51:07 +0100

    added Lina to fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,11 +1,12 @@
 if(!Players.findOne()) {
-  Players.insert({name: 'Kobe',        gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Christoph',   gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Aggel',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Benni',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Hundefrisör', gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Oli',         gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Pan',         gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Daniel',      gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
-  Players.insert({name: 'Roman',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Kobe',        gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Christoph',   gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Aggel',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Benni',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Hundefrisör', gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Oli',         gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Pan',         gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Daniel',      gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Roman',       gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
+  Players.insert({name: 'Lina',        gamesWon: 0, gamesLost: 0, score: 0, goalsMade: 0, goalsAgainst: 0, scoreHistory: [0], winningTeamBuddyCount:{}, lostAgainstCount:{}});
 }
